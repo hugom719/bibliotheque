72e001c961a06a061785951ef7637593cb654f5a
hugom <hugo.mercier@condorcet.be>
2024-02-21 19:39:53 +0100

    up

---
+++ src/bibliotheque/DemoDate.java
@@ -0,0 +1,7 @@
+package bibliotheque;
+import java.time.LocalDate;
+public class DemoDate{
+    public static void main(String[] args) {
+        LocalDate madate = LocalDate.of(2020,1,30);// LocalDate.of(an,m,j);
+        System.out.println(madate);
+    }
